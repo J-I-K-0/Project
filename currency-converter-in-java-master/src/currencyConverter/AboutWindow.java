8cd49b77776a3eed8604dadc1564dd784d6c08ec
J-I-K-0 <106380596+J-I-K-0@users.noreply.github.com>
2023-06-22 18:09:15 +0400

    Update AboutWindow.java

--- currency-converter-in-java-master/src/currencyConverter/AboutWindow.java
+++ currency-converter-in-java-master/src/currencyConverter/AboutWindow.java
@@ -19,26 +19,24 @@ public class AboutWindow extends JFrame {
 	private JPanel contentPane;
 	private static AboutWindow windowInstance = null;
 
-	/**
-	 * Create the aboutWindow frame
-	 */
+	
 	private AboutWindow() {
 		setTitle(BUNDLE.getString("AboutWindow.this.title")); //$NON-NLS-1$
 		setBounds(100, 100, 347, 260);
 		setLocationRelativeTo(null);
 		setResizable( false );
 
-		// Window components
+		
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 
-		// Label "author"
+		
 		JLabel lblAuthor = new JLabel(BUNDLE.getString("AboutWindow.lblAuthor.text")); //$NON-NLS-1$
 		lblAuthor.setHorizontalAlignment(SwingConstants.CENTER);
 		lblAuthor.setBounds(65, 122, 219, 33);
 
-		// label with a clickable link
+		
 		JLabel lblLink = new JLabel("http://jvinceno.fr");
 		lblLink.setBounds(65, 159, 219, 33);
 		lblLink.setHorizontalAlignment(SwingConstants.CENTER);
@@ -56,20 +54,20 @@ public class AboutWindow extends JFrame {
         });
 		contentPane.setLayout(null);
 
-		// label "title"
+		
 		JLabel lblTitle = new JLabel("Currency Converter");
 		lblTitle.setBounds(65, 12, 219, 33);
 		lblTitle.setHorizontalAlignment(SwingConstants.CENTER);
 		lblTitle.setFont(new Font("Noto Sans", Font.BOLD, 15));
 		contentPane.add(lblTitle);
 
-		// label "licence"
+		
 		JLabel lblLicenceMit = new JLabel("Licence GNU GPL v3.0");
 		lblLicenceMit.setBounds(65, 77, 219, 33);
 		lblLicenceMit.setHorizontalAlignment(SwingConstants.CENTER);
 		contentPane.add(lblLicenceMit);
 
-		// label "version"
+	
 		JLabel lblVersion = new JLabel("Version 1.0");
 		lblVersion.setHorizontalAlignment(SwingConstants.CENTER);
 		lblVersion.setBounds(65, 45, 219, 33);
@@ -78,7 +76,6 @@ public class AboutWindow extends JFrame {
 		contentPane.add(lblLink);
 	}
 
-	// Check if the window is already created to launch only one instance of the window.
 	public static AboutWindow getInstance() {
 		if (windowInstance == null) {
 			windowInstance = new AboutWindow();
